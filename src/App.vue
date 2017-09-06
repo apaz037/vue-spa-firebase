a035ec18aa5d3902ba7bdd60a2e13e4b2225e293
Aaron Paz <aaron@aarons-mbp.lan>
2017-09-06 18:28:33 -0400

    top-toolbar implementation

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,16 @@
       </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+        flat
+        v-for="item in menuItems"
+        :key="item.title"
+        :to="item.path">
+        <v-icon left dark>{{ item.icon }}</v-icon>
+        {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
 
     <main>
