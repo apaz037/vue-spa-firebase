88bf747343e485dcf985858a634f4f4001c29f3f
Aaron Paz <aaron@aarons-mbp.lan>
2017-09-06 18:17:02 -0400

    set up initial Home, Landing, Signin, and Signup components, reorganized router

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <h3>Home page</h3>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/components/Signup.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-layout>
+    <v-flex>
+      <h3>Signup page</h3>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {}
+</script>
